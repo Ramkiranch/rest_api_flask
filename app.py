cf299b999fa7f973e3bbfe0e4db888bac7a3bee7
Ramkiranch <vcheve2@uic.edu>
2019-07-24 01:07:56 -0500

    restapi_flask_sqlite

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from user import UserRegister
+from item import Item, GetItem
+
+app = Flask(__name__)
+app.secret_key = 'ramki'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
+
+# items = [] #in memory database
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(GetItem, '/items')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
